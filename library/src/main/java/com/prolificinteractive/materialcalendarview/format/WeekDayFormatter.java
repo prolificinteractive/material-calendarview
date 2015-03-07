e09c27ce0f32880e2798b0536ee589342cbfa04a
Adam Koski <dandc87@gmail.com>
2015-03-06 17:14:39 -0800

    Rename for WeekDay consistancy

---
+++ library/src/main/java/com/prolificinteractive/materialcalendarview/format/WeekDayFormatter.java
@@ -0,0 +1,16 @@
+package com.prolificinteractive.materialcalendarview.format;
+
+import com.prolificinteractive.materialcalendarview.CalendarUtils;
+
+/**
+ * Supply labels for a given day of the week
+ */
+public interface WeekDayFormatter {
+    /**
+     * @param dayOfWeek the day of the week as returned by {@linkplain java.util.Calendar#get(int)} for {@linkplain java.util.Calendar#DAY_OF_YEAR}
+     * @return a label for the day of week
+     */
+    CharSequence format(int dayOfWeek);
+
+    public static final WeekDayFormatter DEFAULT = new CalendarWeekDayFormatter(CalendarUtils.getInstance());
+}
