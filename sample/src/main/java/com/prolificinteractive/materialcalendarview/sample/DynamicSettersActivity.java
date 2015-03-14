ae4f2917bf341eaeab5e5b87e4d5e2243a9ca2e7
dandc87 <dandc87@gmail.com>
2015-03-14 09:25:45 -0700

    Update sample with tileSize customization

--- sample/src/main/java/com/prolificinteractive/materialcalendarview/sample/DynamicSettersActivity.java
+++ sample/src/main/java/com/prolificinteractive/materialcalendarview/sample/DynamicSettersActivity.java
@@ -1,11 +1,14 @@
 package com.prolificinteractive.materialcalendarview.sample;
 
+import android.app.AlertDialog;
 import android.app.DatePickerDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.widget.DatePicker;
+import android.widget.NumberPicker;
 import android.widget.TextView;
 import butterknife.ButterKnife;
 import butterknife.InjectView;
@@ -24,6 +27,7 @@ public class DynamicSettersActivity extends ActionBarActivity implements OnDateC
 
     @InjectView(R.id.textView) TextView textView;
     @InjectView(R.id.calendarView) MaterialCalendarView widget;
+    private int currentTileSize = 44;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -94,6 +98,24 @@ public class DynamicSettersActivity extends ActionBarActivity implements OnDateC
         });
         widget.setArrowColor(color);
         widget.setSelectionColor(color);
+    }
+
+    @OnClick(R.id.button_set_tile_size) void onTileSizeClicked() {
+        final NumberPicker view = new NumberPicker(this);
+        view.setMinValue(24);
+        view.setMaxValue(64);
+        view.setWrapSelectorWheel(false);
+        view.setValue(currentTileSize);
+        new AlertDialog.Builder(this)
+                .setView(view)
+                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        currentTileSize = view.getValue();
+                        widget.setTileSizeDp(currentTileSize);
+                    }
+                })
+                .show();
     }
 
     public static void showDatePickerDialog(Context context, CalendarDay day,
