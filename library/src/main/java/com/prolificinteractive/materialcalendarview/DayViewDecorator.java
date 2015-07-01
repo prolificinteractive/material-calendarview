e9000ac245a9f0bb0e6168f2b8823b746801487d
Adam Koski <dandc87@gmail.com>
2015-06-30 17:19:15 -0700

    Add javadoc for decorator classes

--- library/src/main/java/com/prolificinteractive/materialcalendarview/DayViewDecorator.java
+++ library/src/main/java/com/prolificinteractive/materialcalendarview/DayViewDecorator.java
@@ -6,6 +6,8 @@ package com.prolificinteractive.materialcalendarview;
 public interface DayViewDecorator {
 
     /**
+     * Determine if a specific day should be decorated
+     *
      * @param day {@linkplain CalendarDay} to possibly decorate
      *
      * @return true if this decorator should be applied to the provided day
@@ -13,6 +15,8 @@ public interface DayViewDecorator {
     boolean shouldDecorate(CalendarDay day);
 
     /**
+     * Set decoration options onto a facade to be applied to all relevant days
+     *
      * @param view View to decorate
      */
     void decorate(DayViewFacade view);
