1e06e62c7c17d36458272f0bba770187b3588d00
Adam Koski <dandc87@gmail.com>
2015-06-30 17:14:30 -0700

    Add javadoc for DotSpan

--- library/src/main/java/com/prolificinteractive/materialcalendarview/spans/DotSpan.java
+++ library/src/main/java/com/prolificinteractive/materialcalendarview/spans/DotSpan.java
@@ -4,28 +4,51 @@ import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.text.style.LineBackgroundSpan;
 
+/**
+ * Span to draw a dot centered under a section of text
+ */
 public class DotSpan implements LineBackgroundSpan {
 
-    private static final float DEFAULT_RADIUS = 3;
+    /**
+     * Default radius used
+     */
+    public static final float DEFAULT_RADIUS = 3;
 
     private final float radius;
     private final int color;
 
+    /**
+     * @see #DotSpan(float, int)
+     * @see #DEFAULT_RADIUS
+     */
     public DotSpan() {
         this.radius = DEFAULT_RADIUS;
         this.color = 0;
     }
 
+    /**
+     * @see #DotSpan(float, int)
+     * @see #DEFAULT_RADIUS
+     */
     public DotSpan(int color) {
         this.radius = DEFAULT_RADIUS;
         this.color = color;
     }
 
+    /**
+     * @see #DotSpan(float, int)
+     */
     public DotSpan(float radius) {
         this.radius = radius;
         this.color = 0;
     }
 
+    /**
+     * Create a span to draw a dot using a specified radius and color
+     *
+     * @param radius radius for the dot
+     * @param color color of the dot
+     */
     public DotSpan(float radius, int color) {
         this.radius = radius;
         this.color = color;
