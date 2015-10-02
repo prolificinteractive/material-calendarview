e92f9ba7e9c43bde73d7c6d81b612f60b6132e52
Adam Koski <dandc87@gmail.com>
2015-10-02 09:47:56 -0700

    Cleanup MonthPager

--- library/src/main/java/com/prolificinteractive/materialcalendarview/MonthPager.java
+++ library/src/main/java/com/prolificinteractive/materialcalendarview/MonthPager.java
@@ -2,22 +2,17 @@ package com.prolificinteractive.materialcalendarview;
 
 import android.content.Context;
 import android.support.v4.view.ViewPager;
-import android.util.AttributeSet;
 import android.view.MotionEvent;
 
 /**
- * @author <a href="http://www.lusfold.com" target="_blank">Lusfold</a>
+ * Custom ViewPager that allows swiping to be disabled.
  */
-public class MonthPager extends ViewPager {
+class MonthPager extends ViewPager {
 
     private boolean pagingEnabled = true;
 
     public MonthPager(Context context) {
         super(context);
-    }
-
-    public MonthPager(Context context, AttributeSet attrs) {
-        super(context, attrs);
     }
 
     @Override
