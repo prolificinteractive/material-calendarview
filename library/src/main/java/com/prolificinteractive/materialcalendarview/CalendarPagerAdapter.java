ffd551d8b657f303b4ccb7852e09b7ce9ba7a588
Quentin Colle <quentincolle@Quentins-MacBook-Pro-3.local>
2016-05-10 17:08:42 -0400

    Added api methods for talkback

--- library/src/main/java/com/prolificinteractive/materialcalendarview/CalendarPagerAdapter.java
+++ library/src/main/java/com/prolificinteractive/materialcalendarview/CalendarPagerAdapter.java
@@ -137,7 +137,7 @@ abstract class CalendarPagerAdapter<V extends CalendarPagerView> extends PagerAd
     @Override
     public Object instantiateItem(ViewGroup container, int position) {
         V pagerView = createView(position);
-        pagerView.setContentDescription(pagerView.getContext().getString(R.string.calendar));
+        pagerView.setContentDescription(mcv.getCalendarContentDescription());
         pagerView.setAlpha(0);
         pagerView.setSelectionEnabled(selectionEnabled);
 
