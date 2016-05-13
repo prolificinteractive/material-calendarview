c123024d5ba131f1ef6c16b9456c7c6566b37453
Quentin Colle <quentincolle@Quentins-MacBook-Pro-3.local>
2016-05-13 16:21:58 -0400

    cleanup

--- library/src/main/java/com/prolificinteractive/materialcalendarview/MaterialCalendarView.java
+++ library/src/main/java/com/prolificinteractive/materialcalendarview/MaterialCalendarView.java
@@ -66,9 +66,6 @@ import java.util.List;
  */
 public class MaterialCalendarView extends ViewGroup {
 
-    static volatile State singleton = null;
-    private State state;
-
     /**
      * {@linkplain IntDef} annotation for selection mode.
      *
@@ -219,6 +216,8 @@ public class MaterialCalendarView extends ViewGroup {
     private int selectionMode = SELECTION_MODE_SINGLE;
     private boolean allowClickDaysOutsideCurrentMonth = true;
     private int firstDayOfWeek;
+
+    private State state;
 
     public MaterialCalendarView(Context context) {
         this(context, null);
