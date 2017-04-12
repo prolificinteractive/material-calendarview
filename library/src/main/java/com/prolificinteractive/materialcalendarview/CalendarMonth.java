f40c60113f178213e90234e109a44f0feb239bfc
Hafiq <hafiqiqmal93@gmail.com>
2017-04-12 17:48:12 +0800

    [Month ] modify enum CalendarMonth

--- library/src/main/java/com/prolificinteractive/materialcalendarview/CalendarMonth.java
+++ library/src/main/java/com/prolificinteractive/materialcalendarview/CalendarMonth.java
@@ -8,23 +8,33 @@ import java.util.List;
  */
 
 public enum  CalendarMonth {
-    JANUARY(0),
-    FEBUARY(1),
-    MARCH(2),
-    APRIL(3),
-    MAY(4),
-    JUNE(5),
-    JULY(6),
-    AUGUST(7),
-    SEPTEMBER(8),
-    OCTOBER(9),
-    NOVEMBER(10),
-    DECEMBER(11);
+    JANUARY(0,"Jan"),
+    FEBUARY(1,"Feb"),
+    MARCH(2,"Mac"),
+    APRIL(3,"Apr"),
+    MAY(4,"May"),
+    JUNE(5,"Jun"),
+    JULY(6,"Jul"),
+    AUGUST(7,"Aug"),
+    SEPTEMBER(8,"Sep"),
+    OCTOBER(9,"Oct"),
+    NOVEMBER(10,"Nov"),
+    DECEMBER(11,"Dec");
 
     final int monthNum;
+    final String name;
 
-    CalendarMonth(int x) {
+    CalendarMonth(int x,String name) {
         this.monthNum = x;
+        this.name = name;
+    }
+
+    public static CalendarMonth getMonth(int x){
+        for (CalendarMonth calendarMonth:values()){
+            if (calendarMonth.monthNum == x)
+                return calendarMonth;
+        }
+        return null;
     }
 
     public static List<String> getList(){
