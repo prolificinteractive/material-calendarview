28d2b3663f2119ec417237b53453ffa4f70ba5a2
Quentin Colle <quentin41500@gmail.com>
2017-09-26 12:00:05 -0400

    Chore - Version Update

--- library/src/main/java/com/prolificinteractive/materialcalendarview/CalendarPager.java
+++ library/src/main/java/com/prolificinteractive/materialcalendarview/CalendarPager.java
@@ -1,13 +1,13 @@
 package com.prolificinteractive.materialcalendarview;
 
 import android.content.Context;
-import android.support.v4.view.BetterViewPager;
+import android.support.v4.view.ViewPager;
 import android.view.MotionEvent;
 
 /**
  * Custom ViewPager that allows swiping to be disabled.
  */
-class CalendarPager extends BetterViewPager {
+class CalendarPager extends ViewPager {
 
     private boolean pagingEnabled = true;
 
