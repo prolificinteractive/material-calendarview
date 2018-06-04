73ef677f4205b7afb1cbc12464ab80103789ce90
twiceYuan <twiceyuan@gmail.com>
2018-06-04 11:17:34 +0800

    Update WeekDayFormatter.java

--- library/src/main/java/com/prolificinteractive/materialcalendarview/format/WeekDayFormatter.java
+++ library/src/main/java/com/prolificinteractive/materialcalendarview/format/WeekDayFormatter.java
@@ -9,7 +9,7 @@ public interface WeekDayFormatter {
     /**
      * Convert a given day of the week into a label
      *
-     * @param dayOfWeek the day of the week as returned by {@linkplain java.util.Calendar#get(int)} for {@linkplain java.util.Calendar#DAY_OF_YEAR}
+     * @param dayOfWeek the day of the week as returned by {@linkplain java.util.Calendar#get(int)} for {@linkplain java.util.Calendar#DAY_OF_WEEK}
      * @return a label for the day of week
      */
     CharSequence format(int dayOfWeek);
