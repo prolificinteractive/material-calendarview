152b6d7dd876f1bce2b2c3ddfa7534121f99a92b
Quentin Colle <quentin41500@gmail.com>
2018-07-17 15:58:16 -0400

    Comments

--- library/src/main/java/com/prolificinteractive/materialcalendarview/MaterialCalendarViewInitProvider.java
+++ library/src/main/java/com/prolificinteractive/materialcalendarview/MaterialCalendarViewInitProvider.java
@@ -10,6 +10,9 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import com.jakewharton.threetenabp.AndroidThreeTen;
 
+/**
+ * With this in place, we don't have to have the user install 310Abp by himself.
+ */
 public final class MaterialCalendarViewInitProvider extends ContentProvider {
 
   private static final String MCV_AUTHORITY =
