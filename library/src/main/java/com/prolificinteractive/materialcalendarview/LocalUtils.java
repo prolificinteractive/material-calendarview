2f7a34cb9898449a0f9de6a56c79b0ee5340dc71
Quentin Colle <quentin41500@gmail.com>
2018-07-25 18:14:21 -0400

    Chore - Getter Activity (#826)

--- library/src/main/java/com/prolificinteractive/materialcalendarview/LocalUtils.java
+++ library/src/main/java/com/prolificinteractive/materialcalendarview/LocalUtils.java
@@ -4,15 +4,15 @@ import java.util.Locale;
 
 class LocalUtils {
 
-    private LocalUtils() { }
+  private LocalUtils() { }
 
-    static boolean isRTL() {
-        return isRTL(Locale.getDefault());
-    }
+  static boolean isRTL() {
+    return isRTL(Locale.getDefault());
+  }
 
-    private static boolean isRTL(Locale locale) {
-        final int directionality = Character.getDirectionality(locale.getDisplayName().charAt(0));
-        return directionality == Character.DIRECTIONALITY_RIGHT_TO_LEFT ||
-                directionality == Character.DIRECTIONALITY_RIGHT_TO_LEFT_ARABIC;
-    }
+  private static boolean isRTL(Locale locale) {
+    final int directionality = Character.getDirectionality(locale.getDisplayName().charAt(0));
+    return directionality == Character.DIRECTIONALITY_RIGHT_TO_LEFT ||
+        directionality == Character.DIRECTIONALITY_RIGHT_TO_LEFT_ARABIC;
+  }
 }
