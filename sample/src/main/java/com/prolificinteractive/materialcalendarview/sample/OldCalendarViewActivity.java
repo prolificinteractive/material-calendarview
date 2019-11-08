42763555416a991de1acdfd07d5e1a1cd43ba455
SIMSEUNGMIN <ssm4591@naver.com>
2019-11-09 02:47:09 +0900

    Always print the current date when push the Old Calendar View

--- sample/src/main/java/com/prolificinteractive/materialcalendarview/sample/OldCalendarViewActivity.java
+++ sample/src/main/java/com/prolificinteractive/materialcalendarview/sample/OldCalendarViewActivity.java
@@ -1,6 +1,5 @@
 package com.prolificinteractive.materialcalendarview.sample;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.CalendarView;
