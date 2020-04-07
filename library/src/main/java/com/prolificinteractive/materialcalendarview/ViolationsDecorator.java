30e9912f3f304d0bc8f23a267b197ef57d0fad78
Mina Makram <mina.makram@pharos-solutions.de>
2020-04-07 21:29:41 +0200

    Add violations decorator

---
+++ library/src/main/java/com/prolificinteractive/materialcalendarview/ViolationsDecorator.java
@@ -0,0 +1,32 @@
+package com.prolificinteractive.materialcalendarview;
+
+import android.app.Activity;
+
+import java.util.Collection;
+import java.util.HashSet;
+
+public class ViolationsDecorator implements DayViewDecorator {
+
+    private int[] color;
+    private HashSet<CalendarDay> dates;
+    private Activity context;
+
+    public ViolationsDecorator(Activity context, int[] color, Collection<CalendarDay> dates) {
+        this.color = color;
+        this.dates = new HashSet<>(dates);
+        this.context = context;
+    }
+
+    @Override
+    public boolean shouldDecorate(CalendarDay day) {
+        return dates.contains(day);
+    }
+
+    @Override
+    public void decorate(DayViewFacade view) {
+
+
+        view.addSpan(new CustomMultipleDotSpan(context, 6, color));
+
+    }
+}
