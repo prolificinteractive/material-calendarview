d63314bd9d0c6983b29d14a4f59c5f6c7e96f130
brymher <brymher@yahoo.com>
2020-11-21 05:23:09 +0300

    AnimatorListener - AnimatorListener.kt

---
+++ library/src/main/java/com/prolificinteractive/materialcalendarview/AnimatorListener.kt
@@ -0,0 +1,22 @@
+package com.prolificinteractive.materialcalendarview;
+
+import android.animation.Animator;
+
+class AnimatorListener : Animator.AnimatorListener {
+    override fun onAnimationStart(animation: Animator) {
+
+    }
+
+    override fun onAnimationEnd(animation: Animator) {
+
+    }
+
+    override fun onAnimationCancel(animation: Animator) {
+
+    }
+
+    override fun onAnimationRepeat(animation: Animator) {
+
+    }
+
+}
