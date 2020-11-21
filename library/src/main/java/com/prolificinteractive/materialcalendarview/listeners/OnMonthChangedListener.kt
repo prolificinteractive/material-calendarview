d5a8365b5e20339e069d473dca565a738319f117
brymher <brymher@yahoo.com>
2020-11-21 05:40:14 +0300

    Finished listeners editing.

--- library/src/main/java/com/prolificinteractive/materialcalendarview/listeners/OnMonthChangedListener.kt
+++ library/src/main/java/com/prolificinteractive/materialcalendarview/listeners/OnMonthChangedListener.kt
@@ -8,11 +8,11 @@ import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
  */
 public interface OnMonthChangedListener {
 
-  /**
-   * Called upon change of the selected day
-   *
-   * @param widget the view associated with this listener
-   * @param date the month picked, as the first day of the month
-   */
-  void onMonthChanged(MaterialCalendarView widget, CalendarDay date);
+    /**
+     * Called upon change of the selected day
+     *
+     * @param widget the view associated with this listener
+     * @param date the month picked, as the first day of the month
+     */
+    fun onMonthChanged(widget: MaterialCalendarView, date: CalendarDay);
 }
